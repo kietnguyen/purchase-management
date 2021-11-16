ef2a63412ccc23f2397008396180f4091a1af7ce
Kiet Nguyen <kietnguyen86@gmail.com>
2021-11-16 19:13:26 +0700

    Add Purchase form component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.less';
 import History from './components/History';
 import Layout from './components/Layout';
 import Overview from './components/Overview';
+import Purchase from './components/Purchase';
 
 function App() {
   return (
     <Layout>
       <Overview />
       <History />
+      <Purchase />
     </Layout>
   );
 }
--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -3,4 +3,6 @@ import dayjs from 'dayjs';
 const relativeTime = require('dayjs/plugin/relativeTime');
 dayjs.extend(relativeTime)
 
+export const dateFormat = 'YYYY-MM-DD';
+
 export const dateFormatter = (timestamp) => dayjs.unix(timestamp).fromNow();
