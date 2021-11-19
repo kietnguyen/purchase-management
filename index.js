68c072e417f3598bfb70042e36d6e514679691d6
Kiet Nguyen <kietnguyen86@gmail.com>
2021-11-20 01:28:54 +0700

    Add initial sample data

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const purchases = {};
+const purchases = {
+  'd4681f12-06e2-4730-9114-1e7605e50a90': {
+    id: 'd4681f12-06e2-4730-9114-1e7605e50a90',
+    name: 'Food Processor',
+    price: 100,
+    usageCost: 1,
+    currentUses: 5,
+    expectedUses: 100,
+    purchasedAt: 1636709651,
+  },
+  '569ce2c1-41fe-468a-8135-f4c285ae9a32': {
+    id: '569ce2c1-41fe-468a-8135-f4c285ae9a32',
+    name: 'eReader',
+    price: 200,
+    usageCost: 0.25,
+    currentUses: 301,
+    expectedUses: 300,
+    purchasedAt: 1605519251,
+  },
+};
 
 app.get('/purchases', (req, res) => {
   res.send(Object.values(purchases));
@@ -16,11 +35,11 @@ app.post('/purchases', (req, res) => {
   const purchase = req.body;
   purchases[purchase.id] = purchase;
 
-  res.send(purchase);
+  res.status(201).send(purchase);
 });
 
 app.post('/purchases/:id/use', (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
   const purchase = purchases[id];
   purchase.currentUses++;
 
