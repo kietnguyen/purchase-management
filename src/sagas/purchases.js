e699a2fb6527c476be1139f744c9fd2e8c6c8c4f
Kiet Nguyen <kietnguyen86@gmail.com>
2021-11-19 15:34:27 +0700

    Create a new purchase to remote

--- src/sagas/purchases.js
+++ src/sagas/purchases.js
@@ -11,6 +11,16 @@ function* fetchPurchases() {
   }
 }
 
+function* addPurchase({ payload }) {
+  try {
+    const { purchase } = payload;
+    yield call(axios.post, 'http://localhost:3001/purchases', purchase);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 export default function* purchasesSaga() {
   yield takeLeading(types.FETCH_PURCHASES, fetchPurchases);
+  yield takeLeading(types.ADD_PURCHASE, addPurchase);
 }
