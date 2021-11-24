cd447c88c8d42d8f820af18eeeb6b4445983fe03
Kiet Nguyen <kietnguyen86@gmail.com>
2021-11-25 00:46:29 +0700

    Update relative date for today and yesterday

--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -2,4 +2,14 @@ import moment from 'moment';
 
 export const dateFormat = 'YYYY-MM-DD';
 
-export const relativeDate = (date) => moment(date).fromNow();
+export const relativeDate = (date) => {
+  const currentDate = moment(date);
+  const today = moment();
+  if (currentDate.day() === today.day()) {
+    return 'today';
+  }
+  if (currentDate.day() === today.subtract(1, 'days').day()) {
+    return 'yesterday';
+  }
+  return moment(date).fromNow();
+};
