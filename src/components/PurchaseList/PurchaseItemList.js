cdc0c79765111f30f73a132ead42a3806b49926e
Kiet Nguyen <kietnguyen86@gmail.com>
2021-12-03 15:13:57 +0700

    Seperate static parts and dynamic parts

---
+++ src/components/PurchaseList/PurchaseItemList.js
@@ -0,0 +1,31 @@
+import { List as AntList } from 'antd';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import PurchaseItem from './PurchaseItem';
+
+const List = styled(({ children, ...props }) =>
+  <AntList {...props}>{children}</AntList>)`
+  &.ant-list {
+    .ant-list-item {
+      padding-left: 12px;
+      padding-right: 12px;
+    }
+  }
+`;
+
+const PurchaseItemList = () => {
+  const purchases = useSelector((state) => state.purchases);
+
+  return (
+    <List
+      bordered
+      itemLayout='horizontal'
+      dataSource={purchases}
+      rowKey='id'
+      renderItem={(item) => <PurchaseItem item={item} />}
+    />
+  );
+};
+
+export default PurchaseItemList;
--- src/components/PurchaseList/index.js
+++ src/components/PurchaseList/index.js
@@ -1,40 +1,20 @@
-import { Col, List as AntList, Row, Typography } from 'antd';
+import { Col, Row, Typography } from 'antd';
 import React from 'react';
-import { useSelector } from 'react-redux';
-import styled from 'styled-components';
 import { Content } from '../common/styles';
-import PurchaseItem from './PurchaseItem';
+import PurchaseItemList from './PurchaseItemList';
 
 const { Title } = Typography;
 
-const List = styled(({ children, ...props }) =>
-  <AntList {...props}>{children}</AntList>)`
-  &.ant-list {
-    .ant-list-item {
-      padding-left: 12px;
-      padding-right: 12px;
-    }
-  }
-`;
-
 const PurchaseList = () => {
-  const purchases = useSelector((state) => state.purchases);
-
   return (
     <Content>
       <Title level={2}>History</Title>
       <Row>
         <Col span={24}>
-          <List
-            bordered
-            itemLayout='horizontal'
-            dataSource={purchases}
-            renderItem={(item) => <PurchaseItem key={item.id} item={item} />}
-          />
+          <PurchaseItemList />
         </Col>
       </Row>
     </Content>
-
   );
 };
 
