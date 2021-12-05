8a64c5ee52fbe1de6236fa1cc8961954c5998071
Kiet Nguyen <kietnguyen86@gmail.com>
2021-12-05 12:44:13 +0700

    Remove useless interation after completing its usage

--- src/components/PurchaseList/PurchaseItem.js
+++ src/components/PurchaseList/PurchaseItem.js
@@ -1,4 +1,4 @@
-import { CheckCircleFilled, CheckCircleOutlined } from '@ant-design/icons';
+import { CheckCircleFilled, CheckCircleOutlined, CheckOutlined } from '@ant-design/icons';
 import { List as AntList, Progress, Typography } from 'antd';
 import React, { useMemo } from 'react';
 import { useDispatch } from 'react-redux';
@@ -6,11 +6,9 @@ import { addProductUse } from '../../actions/purchases';
 import PurchaseItemDescription from './PurchaseItemDescription';
 
 const { Item } = AntList;
-const { Link } = Typography;
+const { Link, Text } = Typography;
 
-const itemUsageProgress = (current, total) => {
-  const percent = current / total * 100;
-
+const UsageProgress = ({ percent }) => {
   return (
     percent >= 100 ?
       <CheckCircleOutlined style={{ fontSize: '24px', color: '#1da57a' }} /> :
@@ -18,23 +16,30 @@ const itemUsageProgress = (current, total) => {
   );
 };
 
+const UseButton = ({ id, percent }) => {
+  const dispatch = useDispatch();
+
+  if (percent < 100) {
+    return <Link>
+      <CheckCircleFilled
+        onClick={() => dispatch(addProductUse(id))}
+        style={{ fontSize: '2rem' }}
+      /><br />Use
+    </Link>;
+  }
+
+  return <CheckOutlined style={{ color: '#1da57a', fontSize: '2rem' }} />;
+};
+
 const PurchaseItem = ({ item }) => {
   const dispatch = useDispatch();
+  const percent = item.currentUses / item.expectedUses * 100;
 
   return useMemo(
     () => (
-      <Item
-        actions={[
-          <Link>
-            <CheckCircleFilled
-              onClick={() => dispatch(addProductUse(item.id))}
-              style={{ fontSize: '2rem' }}
-            /><br />Use
-          </Link>,
-        ]}
-      >
+      <Item actions={[<UseButton id={item.id} percent={percent} />]}>
         <Item.Meta
-          avatar={itemUsageProgress(item.currentUses, item.expectedUses)}
+          avatar={<UsageProgress percent={percent} />}
           title={item.name}
           description={<PurchaseItemDescription item={item} />}
         />
